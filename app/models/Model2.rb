27298cebf2be429733addad13fe0fc0fa0f74720
David-Kirsch <davidjkirsch@gmail.com>
2020-07-18 21:04:48 -0400

    Created Movie class so individual movies can be made

--- app/models/Model2.rb
+++ app/models/Model2.rb
@@ -1,5 +1,22 @@
 #class for Model2 goes here
 #Feel free to change the name of the class
-class Model2
+class Movie
+    @@all = []
+    attr_accessor :name, :year, :studio
+    def initialize(name, year, studio)
+        @name = name
+        @year = year
+        @studio = studio
+        save
+    end
+
+    def save
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
 
 end
