a3327505760f1094cd2a3ac8c84760718523518c
David-Kirsch <davidjkirsch@gmail.com>
2020-07-19 13:53:40 -0400

    Added credentials check to validate options 6 & 7

--- tools/console.rb
+++ tools/console.rb
@@ -6,7 +6,7 @@ end
 
 
 
-
+# List of Movies
 titanic = Movie.new("Titanic", 1998, "Touchstone")
 the_help = Movie.new("The Help", 2010, "Touchstone")
 hangover = Movie.new("The Hangover", 2008, "Paramount")
@@ -17,10 +17,11 @@ knives_out = Movie.new("Knives Out", 2019, "Lionsgate")
 hustlers = Movie.new("Hustlers", 2019, "Nuyorican Productions")
 jojo_rabbit = Movie.new("Jojo Rabbit", 2019, "Defender Films")
 
+#List of Movie Theaters
 amc = Theater.new("AMC")
 pheonix = Theater.new("Pheonix")
 
-
+#Importing movies into theater
 amc.new_movie(titanic)
 amc.new_movie(hangover)
 amc.new_movie(hustlers)
@@ -40,31 +41,38 @@ def run
   puts "Please select y/n if you would like to create an account.".blue
   account = gets.chomp
   if(account.downcase == "y")
+    puts "Are you signing up as a Customer or Employee?"
+    status = gets.chomp
     puts "Please enter your name"
     name = gets.chomp
-    puts "Are you a manager of a theater? y/n"
-    mgr = gets.chomp
-    if (mgr.downcase == 'y')
-      puts "Please enter Manager password"
-      passcode = gets.chomp
-      if(passcode.downcase == "owner")
-        puts "Access Granted"
-        mgr = true
-      else
-        puts "Sorry incorrect password"
+    if(status.downcase == "employee")
+      puts "Are you a manager of a theater? y/n"
+      mgr = gets.chomp
+      if (mgr.downcase == 'y')
+        puts "Please enter Manager password"
+        passcode = gets.chomp
+        if(passcode.downcase == "owner")
+          puts "Access Granted"
+          mgr = true
+        else
+          puts "Sorry incorrect password"
+          run
+        end
+      end
+      puts "Are you a Producer? y/n"
+      answer = gets.chomp
+      if(answer.downcase == 'y')
+        puts "Please enter Producer password"
+        passcode = gets.chomp
+        if(passcode.downcase == "producer")
+          puts "Access Granted"
+          prod = true
+        else
+          puts "Sorry inccorrect password."
+          run
+        end
       end
     end
-    puts "Are you a Producer? y/n"
-    answer = gets.chomp
-    if(answer.downcase == 'y')
-      puts "Please enter Producer password"
-      passcode = gets.chomp
-      if(passcode.downcase == "producer")
-        prod = true
-      else
-        puts "Sorry inccorrect password."
-    end
-  end
     user = MovieGoer.new(name, mgr, prod )
     run
 
@@ -88,40 +96,43 @@ def run
     puts "To purchase a ticket for a show, press 3."
     puts "To view all your ticket stubs, press 4"
     puts "To change user, press 5."
-    puts "To add a movie, press 6. (Must have credentials)"
-    puts "To add a movie to your theater, press 7. (Must have credentials)"
+    puts "To add a movie to your theater, press 6. (Must have credentials)"
+    puts "To add a movie, press 7. (Must have credentials)"
+    puts "To check the database of movies, press 8"
     puts "To close this app, press 10."
     selection = gets.chomp.to_i
     if(selection == 1)
-      puts "\nHere is a list of all the movies playing in your area:"
+      puts "\nHere is a list of all the movies playing in your area:".green.bold
       puts user.all_movies
     elsif(selection == 2)
-      puts "\nCurrently there are movies playing at "
+      puts "\nCurrently there are movies playing at ".green.bold
       Theater.all.count do |location, index|
         puts "#{index}. #{location.name}\n"
       end
-      puts "What location would you like to view movies at?"
+      puts "What location would you like to view movies at?".bold
       location = gets.chomp
       theater = Theater.all.find {|theater_location| theater_location.name.upcase == location.upcase}
       if(theater)
-        puts "\n**************Currently playing at #{theater.name} Theater***********"
+        puts "\n**************Currently playing at #{theater.name} Theater***********".red
         puts user.movies_at_theater(theater)
-        puts "-------------------------------------"
+        puts "-------------------------------------".red
       else
-        puts "\nSorry, #{location} Theater does not exist in our database."
+        puts "\nSorry, #{location} Theater does not exist in our database.".bold
       end
     elsif(selection == 3)
-      puts "Please enter the name of the movie you would like to see."
+      puts "Please enter the name of the movie you would like to see.".bold
       show = gets.chomp
-      puts "Enter the location you would like to watch the movie at."
+      puts "Enter the location you would like to watch the movie at.".bold
       location = gets.chomp
       theater = Theater.all.find {|theater_location| theater_location.name.upcase == location.upcase}
       if(theater)
         if(user.see_a_movie(show, theater))
-        puts "You have successfully purchased a ticket for #{show.upcase} at #{location}"
+          puts "⭐️•🎥•⭐️•🎥•⭐️•🎥•⭐️•🎥•⭐️•🎥•⭐️•🎥•⭐️•🎥•⭐️•🎥•⭐️•🎥•⭐️•🎥•⭐️"
+          puts "You have successfully purchased a ticket for #{show.upcase} at #{location.upcase}".bold
+          puts "⭐️•🎥•⭐️•🎥•⭐️•🎥•⭐️•🎥•⭐️•🎥•⭐️•🎥•⭐️•🎥•⭐️•🎥•⭐️•🎥•⭐️•🎥•⭐️"
         end
       else
-        puts "Sorry, that theater does not exist on MovieFone."
+        puts "Sorry, that theater does not exist on MovieFone.".red
       end
     elsif(selection == 4)
       puts user.view_ticket_stubs
@@ -129,14 +140,50 @@ def run
       run
     elsif(selection == 6)
       if(user.mgr == true)
-        puts "Enter name of new movie"
-        movie = gets.chomp
-        Movie.new(movie)
+        puts "Enter name of your movie theater".bold
+        movie_theater = gets.chomp
+        theater = Theater.valid?(movie_theater)
+        if(theater)
+          puts "Enter name of movie you would like to add to your theater".bold
+          movie_title = gets.chomp
+          puts "You entered #{movie_title}. Is that correct? y/n".bold
+          confirm = gets.chomp
+          if(confirm.downcase == "y")
+            movie_record = Movie.exist?(movie_title)
+            if(movie_record)
+            theater.new_movie(movie_record)
+            else
+              puts "Movie does not exist".bold
+            end
+          else
+            puts "Cancelled".red
+          end
+        end
       else
-        puts "You do not have the correct credentials."
+        puts "You do not have the correct credentials.".red
       end
+    elsif(selection == 7)
+      if(user.producer == true)
+        puts "Enter name of Movie".bold
+        name = gets.chomp
+        title = Movie.exist?(name)
+        if(!title)
+          puts "Enter year it was made".bold
+          year = gets.chomp.to_i
+          puts "Enter the production company".bold
+          studio = gets.chomp
+          Movie.new(name, year, studio)
+        else
+          puts "Sorry that title already exists in our records".red
+        end
+      else
+        puts "You do not have the correct credentials.".red
+      end
+    elsif(selection == 8)
+      puts "\n"
+      puts Movie.database
     elsif(selection == 10)
-      puts "Thank you for visiting MovieFone. Have a great day!"
+      puts "Thank you for visiting MovieFone. Have a great day!".bold.blue
       exit(true)
     end
   end
