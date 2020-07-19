9584524047cfa69573e8c100de2b80a41b49530c
amitdesh <amitdesh2014@gmail.com>
2020-07-19 10:08:36 -0400

    Done.

--- app/models/Model2.rb
+++ app/models/Model2.rb
@@ -1,5 +1,27 @@
 #class for Model2 goes here
 #Feel free to change the name of the class
-class Model2
+class Ship
+
+    attr_accessor :name, :brand, :type, :captain, :company
+
+    @@all = []
+
+    def initialize(name, brand, type, captain=nil, company)
+        @name = name
+        @brand = brand
+        @type = type
+        @captain = captain
+        @company = company
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def find_owner
+        self.all.select {|ship| ship.name == self}
+    end
+    
 
 end
--- app/models/Model3.rb
+++ app/models/Model3.rb
@@ -1,5 +1,24 @@
 #class for Model3 goes here
 #Feel free to change the name of the class
-class Model3
-  
+class Company
+
+    attr_accessor :name, :brand, :type, :captain, :company
+
+    @@all = []
+
+    def initialize(name, size, founded, country)
+        @name = name
+        @size = size
+        @founded = founded
+        @country = country
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+
+
+
 end
